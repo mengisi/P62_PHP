9af0ba468740187ae6d4fe9dad9e113229614761
MengXue <xuemon@gmail.com>
2015-05-22 16:01:49 -0400

    devoir

--- Devoirs/jour04_vendredi-22-mai/P62_MengXue_Tableau_temperatures/index.php
+++ Devoirs/jour04_vendredi-22-mai/P62_MengXue_Tableau_temperatures/index.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once 'lib_temperature.php';
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -11,6 +11,13 @@
 <body>
 <div id="main_wrapper">
     <?php
+
+    afficher_temperatures($date_temperature);
+
+    unset($date_temperature['2015-05-25']);
+    unset($date_temperature['2015-05-28']);
+
+    afficher_temperatures($date_temperature);
 
     ?>
 </div>
---
+++ Devoirs/jour04_vendredi-22-mai/P62_MengXue_Tableau_temperatures/lib_temperature.php
@@ -0,0 +1,25 @@
+<?php
+$date_temperature = array(
+    '2015-05-22' => 26.4,
+    '2015-05-23' => 27.4,
+    '2015-05-24' => 28.4,
+    '2015-05-25' => 29.4,
+    '2015-05-26' => 30.4,
+    '2015-05-27' => 31.4,
+    '2015-05-28' => 32.4,
+    '2015-05-29' => 33.4,
+    '2015-05-30' => 34.4,
+    '2015-05-31' => 35.4,
+);
+
+function afficher_temperatures($tb)
+{
+        echo '<table>';
+    echo "<tr><th>Num.</th><th>Date</th><th>Temperature</th></tr>";
+    $count = 1;
+    foreach ($tb as $date => $temperature) {
+        echo "<tr><td>$count</td><td>$date</td><td>$temperature</td></tr>";
+        $count++;
+    }
+    echo '</table>';
+}
